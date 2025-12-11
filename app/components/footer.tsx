90488768d225158f8f7e2fc28ee26cfc968dd0a1
andy mutale <drewmutale@gmail.com>
2025-12-11 13:56:49 +0200

    merge
    rock with it

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,86 +1,44 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, Grid, Stage , Stats, BakeShadows,Preload, Lightformer, ScrollControls } from "@react-three/drei";
+import { OrbitControls, Environment, Grid, Stage , Stats, BakeShadows,Preload} from "@react-three/drei";
 import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
-import * as THREE from 'three';
+
 import { Suspense } from "react";
 import {Toy_ktx2} from "./models/Toy_ktx2";
-import { useErrorBoundary } from 'use-error-boundary'
+
 
 
 
 export default function Footer() {
 
-  const { ErrorBoundary, didCatch, error } = useErrorBoundary();
-
-  if (didCatch) {
-    return (
-      <footer className="relative w-full h-screen flex items-center justify-center bg-black text-red-500">
-        <div className="text-center">
-          <h2 className="text-lg font-bold">Model Error</h2>
-          <p className="text-sm mt-2">{error.message}</p>
-        </div>
-      </footer>
-    );
-  }
+  
   return (
-    <footer className="relative w-full h-screen overflow-hidden bg-black text-white font-inter">
-      <Stats
-        showPanel={0}
-        className="stats-overlay"
-      />
-      <div className="absolute inset-0 z-0">
+    <footer className="relative w-full min-h-screen overflow-visible bg-black text-white font-inter">
+      <Stats showPanel={0} className="stats-overlay"/>
+      <div className="absolute inset-0 pointer ">
         
-        <ErrorBoundary>
-        <Canvas flat shadows camera={{ position: [-15, 0, 10], fov: 20 }}  className="touch-none" >
-          <ScrollControls pages={1}>
-          <fog attach="fog" args={["black", 15, 22.5]} />
-          <Stage intensity={0.5} environment="city" shadows={{ type: "accumulative", bias: -0.001, intensity: Math.PI, }} adjustCamera={false}
-          >
+        
+        <Canvas flat shadows camera={{ position: [-15, 0, 10], fov: 25 }} style={{ height: '100vh' }}  >  
+          <fog attach="fog" args={['black', 15, 22.5]} />
+          <Stage intensity={0.5} environment="city" shadows={{ type: 'accumulative', bias: -0.001, intensity: Math.PI }} adjustCamera={false}>
             <Suspense fallback={null}>
-              
-                <group >  
-                  <BakeShadows/>           
-                   <Toy_ktx2 scale={1} position={[0, 0, 0]} />
-              
-                </group>
-                
-             
-              
+              <group >  
+                <BakeShadows/>           
+                <Toy_ktx2 scale={.5} position={[0, -2, 0]} />
+              </group> 
             </Suspense>
-            
-          </Stage>
-
+            </Stage>
+          <Grid renderOrder={-1} position={[0, -1.85, 0]} infiniteGrid cellSize={0.6} cellThickness={0.6} sectionSize={3.3} sectionThickness={1.5} sectionColor="#8080ff" fadeDistance={30} />
+          <OrbitControls enableZoom={false} enableRotate={false} makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
           <EffectComposer enableNormalPass={false}>
             <Bloom luminanceThreshold={2} mipmapBlur />
             <ToneMapping />
           </EffectComposer>
-
-          <Environment resolution={512}>
-        {/* Ceiling */}
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
-        {/* Sides */}
-        <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-        <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
-        {/* Key */}
-        <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
-      </Environment>
-          <Grid
-            renderOrder={-1} position={[0, -2.85, 0]} infiniteGrid cellSize={0.6} sectionSize={3.3} sectionThickness={1.5} sectionColor={new THREE.Color(0.5, 0.5, 1.0)} fadeDistance={20}
-          />
-         {/* <OrbitControls enableRotate={false} enablePan={false} autoRotate autoRotateSpeed={1.4} enableZoom={false} makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2}
-          />*/}
+          <Environment background preset="sunset" blur={0.8} />
           <Preload all />
-          </ScrollControls>
         </Canvas>
-        </ErrorBoundary>
+     
       </div>
 
       {/*Overlay */}
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import Hero  from './components/hero';
 import Footer from './components/footer';
 
 import Navbar from './components/navbar';
-import gsap from 'gsap'
-import { ReactLenis } from 'lenis/react'
-import { useEffect, useRef } from 'react'
 
 export default function Page() {
 
